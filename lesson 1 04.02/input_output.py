3ffd3ef092ef0e3ae789f5dbd4d29cbc30f4ca44
Micael Gabay <michaelgabay770@gmail.com>
2026-02-04 15:48:23 +0200

    1

--- lesson 1 04.02/input_output.py
+++ lesson 1 04.02/input_output.py
@@ -1,10 +1,11 @@
-# birth_year=int(input("enter your birth year"))
-# current_year=2026
-# age=current_year-birth_year
+# # birth_year=int(input("enter your birth year"))
+# # current_year=2026
+# # age=current_year-birth_year
 
-# print("your age is",age)
+# # print("your age is",age)
 
-num1=int(input("enter first number"))
-num2=int(input("enter secound number"))
+# num1=int(input("enter first number"))
+# num2=int(input("enter secound number"))
 
-print("the average is",(num1+num2)/2)
+# print("the average is",(num1+num2)/2)
+
